6269a4f6a835e50d476e048cbd0b54ed3149cac8
tkleg <thkleger@gmail.com>
2025-10-02 19:03:58 -0400

    added a quadratic solver problem

---
+++ Quadratic/QuadraticGenerator.py
@@ -0,0 +1,15 @@
+import random
+
+testCasesComplex = random.sample(range(1, 16), 3)
+print("Test cases with complex roots:", testCasesComplex)
+for i in range(1, 16):
+    fileNum = str(i).zfill(2)
+    with open(f'Quadratic/input/input{ fileNum }.txt', 'w') as f:
+        a = random.randint(5, 25)
+        b = random.randint(5, 25)
+        max_c = (b**2) // (4*a)
+        if i not in testCasesComplex:
+            c = random.randint(max_c-10, max_c)
+        else: # Make some cases with complex roots
+            c = random.randint(max_c+1, max_c+10)
+        f.write(f'{a} {b} {c}\n')
